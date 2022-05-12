2eca6e6563eb8f7e93bd3d92899ca0fe586803ef
Rafia Sultana <rafiasultana0097@gmail.com>
2022-05-12 10:30:47 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,32 @@
+/*
+1.npm install react-router-dom@6
+2.npm install -D tailwindcss
+3.npx tailwindcss init
+(change the 'content' in tailwing.config.js)
+(@tailwind base;
+@tailwind components;
+@tailwind utilities;
+copyAndPaste this in index.css)
+4.npm i daisyui
+(change plugins in tailwing.config.js )
+import browserrouter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
