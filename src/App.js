38756fe10760dcaf5bb67ea04e0e1f78e3fe158b
Rafia Sultana <rafiasultana0097@gmail.com>
2022-05-12 11:10:56 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Navbar from './Pages/Home/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 function App() {
   return (
     <div>
-      <button class="btn btn-active btn-primary">Button</button>
-      <button class="btn btn-primary">Button</button>
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
